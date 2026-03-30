b53ccfb43fe92834a6744258c1c66dba2b289ae9
khannaanurag1 <khannaanurag1@gmail.com>
2026-03-30 22:11:34 +0530

    First commit in this branch

--- Helper.java
+++ Helper.java
@@ -1,10 +1,7 @@
 public class Helper {
-  private String address;
-    private String name;
+  private String address = "LLYARI";
+    private String name = "DHURANDHAR";
     private String phoneNumber;
     public Helper() {
-       name = "Anurag Khanna";
-       address = NULL;
-       phoneNumber = "888888888";
     }
 }
